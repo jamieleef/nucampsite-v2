6016eed33f545efac24861f3fe09468520687d8b
Jamie Feliu <jamieleefeliu@gmail.com>
2023-05-11 09:19:49 -0400

    Favorites

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -15,7 +15,8 @@ import { useEffect } from "react";
 import { fetchPartners } from "../features/partners/partnersSlice";
 import { fetchCampsites } from "../features/campsites/campsitesSlice";
 import { fetchPromotions } from "../features/promotions/promotionsSlice";
-import { fetchComments } from "../features/comments/commentsSlice";
+import { fetchComments } from "../features/comments/commentsSlice"
+import FavoritesScreen from "./FavoritesScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -104,6 +105,29 @@ const ReservationNavigator = () => {
                     headerLeft: () => (
                         <Icon
                             name='tree'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
+
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name='Favorites'
+                component={FavoritesScreen}   
+                options={({ navigation }) => ({ 
+                    title: 'Favorite Campsiites',
+                    headerLeft: () => (
+                        <Icon
+                            name='heart'
                             type='font-awesome'
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
@@ -218,13 +242,29 @@ const Main = () => {
                      }}
                 />
                 <Drawer.Screen 
-                    name="Reserve Campsite"
+                    name="ReserveCampsite"
                     component={ReservationNavigator}
                     options={{ 
                         title: "Reserve Campsite",
                         drawerIcon: ({ color }) => (
                             <Icon
                                 name='tree'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width:24 }}
+                                color={color}
+                            />
+                        )
+                     }}
+                />
+                <Drawer.Screen 
+                    name="Favorites"
+                    component={FavoritesNavigator}
+                    options={{ 
+                        title: "My Favorites",
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='heart'
                                 type='font-awesome'
                                 size={24}
                                 iconStyle={{ width:24 }}
