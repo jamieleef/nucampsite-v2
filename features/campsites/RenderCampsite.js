39f2b04ead6e2e382e129ed95408dacb6cd91545
Jamie Feliu <jamieleefeliu@gmail.com>
2023-05-23 10:28:03 -0400

    Social Sharing

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -48,6 +48,19 @@ const RenderCampsite = (props) => {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+                title,
+                message: `${title}: ${message} ${url}`,
+                url
+            },
+            {
+                dialogTitle: 'Share ' + title
+            }
+        );
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -88,6 +101,20 @@ const RenderCampsite = (props) => {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name='share'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() =>
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
                         />
                     </View>
                 </Card>
